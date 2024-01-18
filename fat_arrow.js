4089e21a33a4cbf24cb3c24f844af31c2bde7e8a
GgnPatle <gagan17octpatle@gmail.com>
2024-01-19 01:31:02 +0530

    fat_arrow

---
+++ fat_arrow.js
@@ -0,0 +1,14 @@
+
+
+//function declaration old method
+function myFunction(m){ //passing actual argument
+    console.log("hello" +" "+ m +" "+ "how are you");
+}
+//function callingg
+myFunction("gagan");
+
+//function declartion new method
+let myFunction1=(name)=>{
+    console.log("good morning Mr."+" "+name);
+}
+myFunction1("Patle");
